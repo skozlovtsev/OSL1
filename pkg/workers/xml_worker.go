39260ebbb9647ff960907c99a5c1fe02d43742c5
M0HTeP <artem-ryazanov1991@yandex.ru>
2023-10-17 18:35:30 +0300

    xml worker

--- pkg/workers/xml_worker.go
+++ pkg/workers/xml_worker.go
@@ -1,5 +1,10 @@
 package workers
 
+import (
+	"encoding/xml"
+	"os"
+)
+
 type XMLWorker struct {
 	fw *FileWorker
 }
@@ -14,12 +19,32 @@ func (w XMLWorker) Create(path string) error {
 	return w.fw.Create(path)
 }
 
-func (w XMLWorker) Write(path string, data []byte) {
+func (w XMLWorker) Write(path string, data map[string]string) error {
 
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0755)
+
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	content, err := xml.Marshal(data)
+
+	f.Write(content)
+
+	return err
 }
 
-func (w XMLWorker) Read(path string) ([]byte, error) {
-	return w.fw.Read(path)
+func (w XMLWorker) Read(path string) (map[string]string, error) {
+
+	content := make(map[string]string)
+
+	data, _ := w.fw.Read(path)
+
+	err := xml.Unmarshal(data, &content)
+
+	return content, err
 }
 
 func (w XMLWorker) Delete(path string) error {
